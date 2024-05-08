8e9060ab28e707ddf03f1368aa454769e74083ab
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-05-08 18:49:33 +0300

    Added collection view

--- Alphabet/ViewController.swift
+++ Alphabet/ViewController.swift
@@ -8,11 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let collection: UICollectionView = {
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configure()
     }
+    
+    private func configure() {
+        view.backgroundColor = .white
+        
+        configureCollectionView()
+    }
+    
+    private func configureCollectionView() {
+        view.addSubview(collection)
+        
+        NSLayoutConstraint.activate([
+            collection.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collection.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collection.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collection.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        collection.dataSource = self
+    }
+}
 
-
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.contentView.backgroundColor = .gray
+        return cell
+    }
 }
 
